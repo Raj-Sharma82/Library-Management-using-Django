43122e1b102f612b3eefe0c3b37d867e8f43dfbb
Raj-Sharma82 <rajsharma20004@gmail.com>
2025-06-24 14:57:33 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,15 +1,24 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for library_mgmt project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.book_list, name='book_list'),
-    path('add/', views.book_create, name='book_create'),
-    path('edit/<int:pk>/', views.book_update, name='book_update'),
-    path('delete/<int:pk>/', views.book_delete, name='book_delete'),
-    path('issue/', views.issue_list, name='issue_list'),
-    path('issue/add/', views.issue_book, name='issue_book'),
-    path('issue/return/<int:pk>/', views.return_book, name='return_book'),
-
-
+    path('admin/', admin.site.urls),
+    path('library/', include('library.urls')),
 ]
-
